dcea3da7c07d5f3a88533f85e4283c5d658da92e
yezhiqiang <yezhiqiang@billionway.cn>
2022-03-05 14:09:03 +0800

    fix

--- contracts/w2_2/Teacher.sol
+++ contracts/w2_2/Teacher.sol
@@ -2,9 +2,32 @@
 
 pragma solidity ^0.8.0;
 
+interface IScore {
+    function addStudent(address studentAddr, string memory name, uint8 score) external;
+    function changeScores(string memory name, uint score) external;
+}
 
 contract Teacher {
-    function Teacher(){
+    address admin;
+    address scoreAddr;
 
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "only admin can operate!");
+    }
+
+    constructor () {
+        msg.sender == admin;
+    }
+
+    function setScoreAddress(address _scoreAddr) external onlyAdmin {
+        scoreAddr = _scoreAddr;
+    }
+
+    function addStudent(address studentAddr, string memory name, uint8 score) external onlyAdmin {
+        IScore(scoreAddr).addStudent(studentAddr, name, score);
+    }
+
+    function changeScores(string memory name, uint score) external onlyAdmin {
+        IScore(scoreAddr).changeScores(name, score);
     }
 }
